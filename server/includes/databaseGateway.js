4b00ff71746678de971a4985473f98f7ae23f48a
Shubham Vishwakarma <sv615218@gmail.com>
2022-06-07 10:33:50 +0530

    db chanhe

--- server/includes/databaseGateway.js
+++ server/includes/databaseGateway.js
@@ -1,9 +1,17 @@
 const
     lowdb = require('lowdb'),
     FileSync = require('lowdb/adapters/FileSync'),
-    path = require('path'),
-    adapter = new FileSync(path.join(__dirname, '../files/maindb.json')),
-    db = lowdb(adapter);
+    path = require('path');
+
+const S3 = require('@sadorlovsky/lowdb-s3')
+const adapter = new S3({ bucket: 'bucket', key: 'db.json' }, {
+    accessKeyId: 'AKIASBGNYCSMLKKUTOXO',
+    secretAccessKey: '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',
+    region: 'ap-south-1',
+    endpoint: 'https://s3.ap-south-1.amazonaws.com'
+})
+
+const db = await low(adapter);
 
 db.defaults({
     admin: {
